8ab1cb7daebd3ae1e596e3ed2b050b9561c711e8
Shawn Becker <sbecker11@me.com>
2024-11-13 03:06:28 -0700

    grqq_app total length 19K exceeds max_tokens 8K

--- groq_app.py
+++ groq_app.py
@@ -1,6 +1,6 @@
 """
-Module for processing a resume document and 
-extracting json-schema formatted output 
+Module for processing a resume document and
+extracting json-schema formatted output
 using Groq's latest LLM
 """
 
@@ -8,14 +8,14 @@ import os
 import json
 import logging
 import sys
+from typing import Optional
 from devtools import debug
 from dotenv import load_dotenv
+from pydantic import ValidationError as PydanticValidationError
+from groq import Groq
 from src.content_loader import load_docx_data
 from src.json_schema_factory import JsonSchemaFactory
 from src.pydantic_resume import PydanticResume
-from pydantic import ValidationError as PydanticValidationError
-from groq import Groq
-from typing import Optional
 
 load_dotenv()
 
@@ -24,56 +24,83 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-def groq_process_resume_text(resume_content, resume_schema) -> Optional[dict]:
+def groq_process_resume_text(resume_content :str, resume_schema_str: str) -> Optional[dict]:
+    """
+    Extracts a JSON object from a resume text using Groq's latest LLM.
 
-    if isinstance(resume_schema, dict) is False:
-        # pretty-printed json schema works best 
-        resume_schema = json.dumps(resume_schema, indent=2)
-        
-    groq_client = Groq(api_key=os.getenv("GROC_API_KEY"))
-    groq_client_model = "meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo"
-    
+    Args:
+        resume_content (str): _description_
+        resume_schema_str (str): _description_
+
+    Raises:
+        ValueError: _description_
+
+    Returns:
+        Optional[dict]: _description_
+    """
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a recipe database that outputs recipes in JSON.\n"
+            f" The JSON object must use the schema: {resume_schema_str}"
+        },
+        {
+            "role": "user",
+            "content": f"Resume Text: \\n------\\n{resume_content}\\n------"
+        }
+    ]
+
+    response_format = {
+        "type": "json_object",
+        "schema": resume_schema_str,
+    }
+
+    # Ensure the total length is within the model's sequence length limit
+    max_tokens = 8192
+    total_length = len(resume_content) + len(resume_schema_str)
+    if total_length > max_tokens:
+        raise ValueError("The combined length of resume content \
+            and schema: %d exceeds the model's sequence length \
+                limit: %d.", total_length, max_tokens)
+
+
+    groq_client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+    groq_client_model = "llama3-groq-8b-8192-tool-use-preview"
+
     completion = groq_client.chat.completions.create(
         model=groq_client_model,
-        messages=[
-            {
-                "role": "system",
-                "content": "You are a recipe database that outputs recipes in JSON.\n"
-                f" The JSON object must use the schema: {resume_schema}"
-            },
-            {
-                "role": "user",
-                "content": f"Resume Text: \\n------\\n{resume_content}\\n------"
-            }
-        ],
+        messages=messages,
         temperature=0,
-        max_tokens=9024,
-        top_p=1,
+        max_tokens=max_tokens,
         stream=False,
-        response_format={
-            "type": "json_object",
-            "schema": resume_schema,
-        },
+        response_format=response_format,
         stop=None,
     )
-    
+
     json_str = ""
     for chunk in completion:
         json_str += chunk.choices[0].delta.content
-    
+
     extracted_object = json.loads(json_str)
     return extracted_object
 
 
 if __name__ == "__main__":
-    
+
     resume_docx_path = os.getenv("RESUME_DOCX_PATH")
     resume_schema_path = os.getenv("RESUME_SCHEMA_PATH")
     test_data_object_path = os.getenv("TEST_DATA_OBJECT_PATH")
 
     # validate the resume schema
+    if not resume_schema_path:
+        logging.error("Error: RESUME_SCHEMA_PATH is not set")
+        sys.exit(1)
+
     try:
-        factory = JsonSchemaFactory(resume_schema_path, test_data_object_path)
+        factory = JsonSchemaFactory(
+            resume_schema_path, 
+            test_data_object_path)
+        
     except json.JSONDecodeError as e:
         logging.error("Error: %s", e)
         sys.exit(1)
@@ -86,46 +113,47 @@ if __name__ == "__main__":
 
     # get the validated resume schema
     resume_schema = factory.get_validated_json_schema()
-    
+    resume_schema_str = json.dumps(resume_schema, indent=2)
+
     # Load the resume content from the DOCX file
     resume_text = load_docx_data(resume_docx_path)
 
-    ## call groq_process_resume_text to extract the resume data 
+    ## call groq_process_resume_text to extract the resume data
     ## from the resume text using groq_client_model the resume schema
     groq_extracted_object = groq_process_resume_text(
         resume_content=resume_text,
-        resume_schema=resume_schema)
+        resume_schema_str=resume_schema_str)
 
     if groq_extracted_object is None:
         logging.error("Error groq_extracted_object is null")
         sys.exit(1)
-    
-    # validate the groq_extracted_pbkect against the resume schema 
+
+    # validate the groq_extracted_object against the resume schema
     if factory.validate_instance(groq_extracted_object) is False:
         logging.error("Error groq_extracted_object does not conform to the resume_schema")
         sys.exit(1)
 
     logging.info("SUCCESS!! groq_extracted_object conforms to the resume_schema!!")
-    
+
     # rename the groq_extracted_data to groq_resume_object
     groq_resume_object = groq_extracted_object
     logging.info("groq_resume_object:")
     logging.info(json.dumps(groq_resume_object, indent=2))
- 
+
     # validate the groq_resume_object against the PydanticResume model
     # by testing if a pudandic_resume_object is created without errors
     try:
         pydantic_resume_object = PydanticResume(**groq_resume_object)
-        
+
         # pydantic models are not json serializable, so using devtools.debug instead
         logging.info("pydantic_resume_object:")
         debug(pydantic_resume_object)
-        
+
         print("SUCCESS groq_resume_object is valid against the PydanticResume model")
 
     except PydanticValidationError as e:
         print("groq_resume_object is invalid against the PydanticResume model.")
         print(str(e))
         exit(1)
-        
+
 print("!!! DONE !!!")
--- src/pydantic_resume.py
+++ src/pydantic_resume.py
@@ -49,60 +49,7 @@ class PydanticResume(BaseModel):
 
 
 if __name__ == "__main__":
-    
+
     pydantic_json_schema = PydanticResume.model_json_schema()
     print("Pydantic JSON schema:")
     print(json.dumps(pydantic_json_schema, indent=2))
-    exit()
-
-    # # Load the JSON data
-    # with open('test-data-object.json', 'r') as data_file:
-    #     data = json.load(data_file)
-
-    # # Extract the json resume data
-    # resume_obj = data.get("resume")
-    
-    # # create the PydanticResume object from the json resume data
-    # try:
-    #     pydantic_resume = PydanticResume(**resume_obj)
-    #     print("resume_obj is valid and the PydanticResume object has been created.")
-        
-    #     print("PydanticResume object:")
-    #     debug(pydantic_resume)
-        
-    #     print("resume_obj:")
-    #     print(json.dumps(resume_obj, indent=2))
-        
-    # except ValidationError as e:
-    #     print("JSON data is invalid against the Pydantic model.")
-    #     print(e.json())
-    #     exit(1)
-    
-    
-
-        
-    # """
-    # # compare JSON-serialized values
-    # # Convert PydanticResume object to string and replace characters
-    # pydantic_resume_str = str(pydantic_resume).\
-    #     replace("'", "").replace("=", ":")
-    
-    # # Convert resume_obj object to string and replace characters
-    # json_resume_str = json.dumps(resume_obj).\
-    #     replace('"', "").replace(": ", ":").\
-    #         replace("{","(").replace("}",")")
-
-    # if pydantic_resume_str == json_resume_str:
-    #     print("The PydanticResume str matches the JSON Resume str")
-    # else:
-    #     print("The PydanticResume str does not match the resume_obj str")
-    #     print("PydanticResume str:")
-    #     print(pydantic_resume_str)
-    #     print("resume_obj str:")
-    #     print(json_resume_str)
-
-    #     PydanticResume str:
-    #     contactInformation:ContactInformation(firstName:Joe, lastName:Jones, email:joe@jones.com, phone:555-555-5555, address:123 Main St, city:San Francisco, state:CA, zipCode:94105, country:USA)  workHistory:[ WorkHistoryItem(workPositionOrTitle:clerk, workForCompanyName:company1, workLocationOrRemote:remote, duration:Duration(start:2019-01-01, end:2020-01-01), workResponsibilitiesAccomplishments:[accomplishment1, accomplishment2]), WorkHistoryItem(workPositionOrTitle:manager, workForCompanyName:company2, workLocationOrRemote:remote, duration:Duration(start:2019-01-01, end:2020-01-01), workResponsibilitiesAccomplishments:[accomplishment3, accomplishment4])]  educationHistory:[EducationHistoryItem(institution:school1, degree:BS, majors:[Computer Science], minors:[Math], gradePointAverage:3.5, duration:Duration(start:2015-01-01, end:2019-01-01)), EducationHistoryItem(institution:school2, degree:MS, majors:[Data Science], minors:[Statistics], gradePointAverage:3.8, duration:Duration(start:2019-01-01, end:2021-01-01))]  skills:[skill1, skill2, skill3] certifications:[cert1, cert2, cert3] publications:None patents:None websites:[site1, site2]
-    #     resume_obj str:
-    #     (contactInformation:                  (firstName:Joe, lastName:Jones, email:joe@jones.com, phone:555-555-5555, address:123 Main St, city:San Francisco, state:CA, zipCode:94105, country:USA), workHistory:[                (workPositionOrTitle:clerk, workForCompanyName:company1, workLocationOrRemote:remote, duration:        (start:2019-01-01, end:2020-01-01), workResponsibilitiesAccomplishments:[accomplishment1, accomplishment2]),                (workPositionOrTitle:manager, workForCompanyName:company2, workLocationOrRemote:remote, duration:        (start:2019-01-01, end:2020-01-01), workResponsibilitiesAccomplishments:[accomplishment3, accomplishment4])], educationHistory:[                    (institution:school1, degree:BS, majors:[Computer Science], minors:[Math], gradePointAverage:3.5, duration:        (start:2015-01-01, end:2019-01-01)),                     (institution:school2, degree:MS, majors:[Data Science], minors:[Statistics], gradePointAverage:3.8, duration:        (start:2019-01-01, end:2021-01-01))], skills:[skill1, skill2, skill3]                               certifications:[cert1, cert2, cert3],                               websites:[site1, site2] 
-    # """
